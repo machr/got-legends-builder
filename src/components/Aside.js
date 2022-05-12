e6790d07bdfb82a6d8992014950cf4fcf1bcf238
Mark Powell <hi@powellmark.com>
2022-05-12 16:25:45 +1000

    Basic grid layout; display character list

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,11 +1,41 @@
+import { useState } from 'react';
+
 import styled from 'styled-components';
+import { TECHNIQUES } from './data'
 
 const Aside = () => {
+  const [selectedCharacter, setSelectedCharacter] = useState('hunter');
+
+  const handleClick = (event) => {
+    // event.preventDefault()
+    const { value } = event.currentTarget;
+    setSelectedCharacter(value)
+  }
+
+  const listCharacters = TECHNIQUES.map((character, index) =>
+    <button type='button' onClick={handleClick} value={character.name} key={index}>{character.name}</button>
+  )
+
+  const characterTechniques = TECHNIQUES.map((character, index) =>
+    <li key={index}>{character.techniques[0].options[0].title}</li>
+  )
+
+
+
   return (
     <aside>
       <AsideHeader>
-        Hello
+        Hello: Current character is: {character}
       </AsideHeader>
+      <AsideBody>
+        <CharacterList>
+          {listCharacters}
+        </CharacterList>
+        <ul>
+          {characterTechniques}
+        </ul>
+      </AsideBody>
+
     </aside>
   )
 }
@@ -15,6 +45,17 @@ const AsideHeader = styled.div`
   background-color: maroon;
   color: white;
 `;
+
+const AsideBody = styled.section`
+  background: #350d0b;
+  color: white;
+`;
+
+const CharacterList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem 2rem;
+`
 
 export { Aside }
 
---
+++ src/components/data.js
@@ -0,0 +1,80 @@
+export const TECHNIQUES = [
+  {
+    "name": "hunter",
+    "techniques": [
+      {
+        "name": "techniques1",
+        "options": [
+          {
+            "title": "Staggering Arrow",
+            "value": "stagger"
+          },
+          {
+            "title": "Explosive Arrow",
+            "value": "explosive"
+          },
+          {
+            "title": "Spirit Archer",
+            "value": "spirit-archer"
+          }
+        ]
+      },
+      {
+        "name": "techniques2",
+        "options": [
+          {
+            "title": "Executioner",
+            "value": "executioner"
+          },
+          {
+            "title": "Ability Cooldown",
+            "value": "ability-cooldown"
+          },
+          {
+            "title": "Extra Legendary",
+            "value": "extra-legendary"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "samurai",
+    "techniques": [
+      {
+        "name": "techniques1",
+        "options": [
+          {
+            "title": "Spirit Pull",
+            "value": "stagger"
+          },
+          {
+            "title": "Raging Flame",
+            "value": "explosive"
+          },
+          {
+            "title": "Explosive Blade",
+            "value": "spirit-archer"
+          }
+        ]
+      },
+      {
+        "name": "techniques2",
+        "options": [
+          {
+            "title": "Executioner",
+            "value": "executioner"
+          },
+          {
+            "title": "Ability Cooldown",
+            "value": "ability-cooldown"
+          },
+          {
+            "title": "Extra Legendary",
+            "value": "extra-legendary"
+          }
+        ]
+      }
+    ]
+  }
+]
